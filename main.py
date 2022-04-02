2ed8baa517c47beb9b18794c565029ac552bc937
Maltaz <mnapieraczz@gmail.com>
2022-04-02 18:07:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+
+
+class FetchWeatherOpenweathermap:
+
+    def __init__(self):
+
+        #CONSTANTS
+        self.api_url: str = 'https://api.openweathermap.org/data/2.5/weather'
+        self.api_key: str = '1b09454cddeb98291a1b4a10f19d77d1' #DELETE THIS
+
+        #GET
+        self.city: str = ""
+
+        #PREPARE
+        self.request_url: str = ""
+
+    def main(self) ->bool:
+        """Docstring
+        """
+        if not self.get_weather_info():
+            print("Error ........")
+        else:
+            return True
+        return False
+
+    def get_weather_info(self):
+        self.prepare_request_url()
+        response = requests.get(self.request_url)
+
+        if response.status_code == 200:
+            weather_data = response.json()
+            weather = weather_data.get('weather', [])[0].get('description', '')
+            temperature = round(weather_data.get("main", {}).get("temp") - 273.15, 2)
+            print(f"Weather: {weather}")
+            print(f"Temperature: {temperature} celsius")
+            return True
+        return False
+
+    def prepare_request_url(self) -> bool:
+        if not self.request_url:
+            self.request_url = f"{self.api_url}?appid={self.api_key}&q={self.get_city_name()}"
+        return True
+
+    @staticmethod
+    def get_city_name() -> str:
+        """Get city name from user
+
+        :return str: entered city name
+        """
+        city = input("Enter city name: ")
+        return city
+
+
+weather = FetchWeatherOpenweathermap()
+weather.main()
